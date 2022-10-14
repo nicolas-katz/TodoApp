6bd39b72b9b952a51f216e600168c2b40aa1d466
nicolas-katz <83619866+nicolas-katz@users.noreply.github.com>
2022-10-14 20:11:07 -0300

    subiendo proyecto terminado

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,84 @@
+import React,{ useState, useEffect } from 'react';
+import '../styles/TodoApp.css';
+import TodoList from './TodoList';
+
+export default function TodoApp() {
+    const [title, setTitle] = useState('');
+    const [todos, setTodos] = useState(JSON.parse(window.localStorage.getItem('todos')) || []);
+
+    useEffect(()=> {
+        console.log(todos)
+    }, [])
+
+    const handleChange = (e) => {
+        setTitle(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(!title) {
+            alert('Do u have to write a correct value!');
+        } else {
+            const date = new Date().toDateString();
+            const newTodo = {
+                id: crypto.randomUUID(),
+                date,
+                title
+            };
+    
+            const temp = [...todos];
+            temp.unshift(newTodo);
+    
+            window.localStorage.setItem('todos', JSON.stringify(temp));
+            setTodos(temp);
+
+            document.querySelector('form input').value = '';
+        }
+    }
+    
+    const handleDelete = (id) => {
+        const todosWithoutDeleted = todos.filter((todo) => todo.id !== id);
+
+        window.localStorage.setItem('todos', JSON.stringify(todosWithoutDeleted));
+        setTodos(todosWithoutDeleted);
+    }
+   
+    const handleEdit = (id, newTitle) => {
+        const todoToEdit = todos.find((todo) => todo.id === id);
+        todoToEdit.title = newTitle;
+
+        window.localStorage.setItem('todos', JSON.stringify(todos));
+        setTodos(todos);
+    }
+
+    return (
+        <div className='todoContainer'>
+            <h1>TodoApp w/ use-State</h1>
+            <div className="todoSubContainer">
+                <form className='createTodoForm' onSubmit={handleSubmit}>
+                    <input 
+                        onChange={(e)=> handleChange(e)}
+                        type="text" 
+                        name="title" 
+                        id="title"
+                        placeholder='Enter your task...' 
+                        required />
+                    <input 
+                        onClick={handleSubmit}
+                        type="submit" 
+                        value="Create Todo" />
+                </form>
+                <div className="todoList">
+                    { 
+                        todos.length > 0 ? 
+                            todos.map( todo => (
+                                <TodoList key={todo.id} todo={todo} handleDelete={handleDelete} handleEdit={handleEdit} />
+                        ))  : 
+                                <h2 className='noTask'>There is no pending task...</h2>
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+function TodoList({todo, handleDelete, handleEdit}) {
+
+  const [isEdit, setIsEdit] = useState(false);
+  const [title, setTitle] = useState('');
+
+  const handleClick = () => {
+    handleDelete(todo.id)
+  }
+  
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  }
+
+  const handleUpdate = () => {
+    handleEdit(todo.id, title)
+  }
+  
+  return (
+    <div className='todosList'>
+        {
+          !isEdit ? 
+          <>
+            <div className="todosData">
+              <h4>{todo.date}</h4>
+              <h5>|</h5>
+              <h3>{todo.title}</h3>
+            </div>
+            <div className="todosButtons">
+              <button onClick={()=> setIsEdit(true)}>Edit</button>
+              <button onClick={handleClick}>Delete</button>
+            </div> 
+          </> :
+          <>
+            <form className='isEditForm'>
+              <input 
+                onChange={handleChange}
+                type="text" 
+                name="title" 
+                id="title"
+                placeholder='Edit your task...' 
+                required />
+              <input 
+                onClick={handleUpdate}
+                type="submit" 
+                value="Update" />
+            </form>
+          </> 
+        }
+    </div>
+  );
+}
+
+export default TodoList;
